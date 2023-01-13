01927f31125d480300ef075449882930284ba98d
Erik M Baxstrom <baxstrem@gmail.com>
2023-01-13 14:46:30 -0800

    remove console log

--- src/components/Phrases/Phrases.js
+++ src/components/Phrases/Phrases.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Phrases.css';
 
 export default function Phrases({ catchphrases }) {
-  console.log('phrases.js has:', catchphrases);
   return (
     <div className="phrases">
       <p>Catchphrases go here</p>
